01c3c697e1952f1e0c1c1f9540c920fa746bc384
Tulasi Kota <nagavenkatatulasikumarikota1@t-mobile.com>
2022-01-22 10:38:05 +0530

    Updated with Unit Testing

--- FactoryandSingleTonPattern/src/Executor.java
+++ FactoryandSingleTonPattern/src/Executor.java
@@ -3,12 +3,12 @@ import org.testng.annotations.Test;
 public class Executor {
 	
 	@Test
-	public void testFetchDetails() {
+	public void testGetRequirement() {
 		SingleTonMain SM = SingleTonMain.getrunner();
 	}
 	
 	@Test
-	public void testGetRequirement() {
+	public void testFetchDetails() {
 		SingleTonMain sm = SingleTonMain.getDetails();
 	}
 }
